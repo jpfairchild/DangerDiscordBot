8f46452418ab1b7d8e56842997d1ab1d6178a23c
JP Fairchild <jpfairchild73@gmail.com>
2018-04-14 16:16:52 -0700

    setup
    comment out

--- Dangerbot.py
+++ Dangerbot.py
@@ -6,7 +6,10 @@ import discord
 from discord.ext import commands
 from discord.ext.commands.bot import _mention_pattern, _mentions_transforms
 
-from . import colors, embeds
-from .bot_utils import config
-from .messages import Messages
+# from . import colors, embeds
+# from .bot_utils import config
+# from .messages import Messages
 import traceback, datetime
+
+comm = dir(discord)
+print (comm)
