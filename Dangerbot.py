cda8809877270452da5ce5ac18701f33a719d199
JP Fairchild <jpfairchild73@gmail.com>
2018-04-15 20:08:49 -0700

    almost nothing
    nothing

--- Dangerbot.py
+++ Dangerbot.py
@@ -1,5 +1,6 @@
 # These are the dependecies. The bot depends on these to function, hence the name. Please do not change these unless your adding to them, because they can break the bot.
 import discord
+# import safygiphy
 import asyncio
 from discord.ext.commands import Bot
 from discord.ext import commands
@@ -7,6 +8,9 @@ import platform
 
 # Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
 client = Bot(description="DangerBot", command_prefix="$", pm_help=False)
+
+# ever = dir(safygiphy)
+# print(ever)
 
 
 @client.event
@@ -17,17 +21,16 @@ async def on_ready():
   print('--------')
   print('Use this link to invite {}:'.format(client.user.name))
   print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
-  return await client.change_presence(game=discord.Game(name='v3 please'))  # This is buggy, let us know if it doesn't work.
+  return await client.change_presence(game=discord.Game(name='Tensor Flow 2.3.4'))  # This is buggy, let us know if it doesn't work.
 
 
 @client.event
 async def on_message(message):
   if message.content.startswith('$spencer'):
-    await client.send_message(message.channel, 'SPENCER MEK NOW')
     imageURL = "http://1.bp.blogspot.com/-jjrcg9oIe4M/UhMzgSuHdrI/AAAAAAAABSY/nrEQA4v3OwI/s1600/Dota+2+Mekanism.bmp"
     embed = discord.Embed()
     embed.set_image(url=imageURL)
-    await client.send_message(message.channel, embed=embed)
+    await client.send_message(message.channel, 'SPENCER MEK NOW', embed=embed)
     msg = await client.wait_for_message(timeout=5, content='lol')
     await client.send_message(message.channel, 'Yea, spencer let me die in Dota so many times')
 
@@ -44,5 +47,12 @@ async def on_message(message):
       embed.set_image(url=imageURL)
       await client.send_message(message.channel, embed=embed)
 
+  # if message.content.startswith('$cat'):
+  #   await client.send_message(message.channel, 'Meow', embed='http://thecatapi.com/api/images/get?format=src&type=gif')
+
+    # imageURL = "http://thecatapi.com/api/images/get?format=src&type=gif"
+    # embed = discord.Embed()
+    # embed.set_image(url=imageURL)
+    # await client.send_message(message.channel, 'Meow', embed=embed)
 
 client.run('NDA3MzA5NDQwMzEyOTM0NDEw.DbRUGw.lL3KVWP_8PDd7Ljou1FTQo-biQU')
